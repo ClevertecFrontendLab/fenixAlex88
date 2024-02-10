2bdc0d107e6b79f86a58e34c5fa22b21221bb7e8
Aleksey Pekar <fenix_al@tut.by>
2024-02-10 16:58:33 +0300

    feat: add main page layout

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,17 @@
+import { Layout } from 'antd';
+
 import './main-page.css';
 
+const { Header, Sider, Content } = Layout;
+
 export const MainPage: React.FC = () => {
-    return <>Main page</>;
+    return (
+        <Layout>
+            <Sider>Навигационное меню</Sider>
+            <Layout>
+                <Header>Header сайта</Header>
+                <Content>Контентная часть</Content>
+            </Layout>
+        </Layout>
+    );
 };
